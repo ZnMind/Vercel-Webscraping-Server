518fa8ab4a53358701e821094bf1d7eec18f37d6
DKProtocol <damann1072@gmail.com>
2022-11-15 16:16:13 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,15 @@
+### Easy Webscraping Server
+
+Easy to use server for webscraping in Javascript. All that is required is a url and an html string sent in the request body. The server then runs a headless chromium instance to scrape the url provided for the html string provided.
+
+Example request for scraping cfb games from ESPN.com:
+```
+var url = 'https://www.espn.com/college-football/schedule/_/week/1/year/2022/seasontype/2';
+var htmlString = '.Table tbody tr .teams__col a';
+
+fetch(`http://localhost:5000/scrape?value=${url}&value=${htmlString}`)
+            .then(res => res.json())
+            .then(body => {
+                return body.data;
+            });
+```
---
+++ server.js
@@ -0,0 +1,74 @@
+import puppeteer from 'puppeteer';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+class Scraper {
+    constructor(url, htmlString) {
+        this.url = url;
+        this.htmlString = htmlString;
+    }
+
+    async startBrowser() {
+        try {
+            console.log("Opening the browser...");
+            let browser = await puppeteer.launch({
+                args: ["--no-sandbox"]
+            });
+            return browser;
+        } catch (err) {
+            console.log("Could not create a browser instance => : ", err);
+        }
+    }
+
+    async findPage(browser) {
+        try {
+            let page = await browser.newPage();
+            return page;
+        } catch (err) {
+            console.log("Oops", err);
+        }
+    }
+
+    async scrapePage(page) {
+        try {
+            console.log(`Navigating to ${this.url}...`);
+            await page.goto(this.url, { waitUntil: 'load', timeout: 0 });
+            let team = await page.$$eval(this.htmlString, res => {
+                res = res.map(el => el.innerText)
+                return res;
+            })
+            return team;
+        }
+        catch (err) {
+            console.log("Could not resolve the browser instance => ", err);
+        } finally {
+            await page.close();
+        }
+    }
+}
+
+app.get("/", (req, res) => {
+    res.json({ status: "Server is running!" });
+});
+
+app.get("/scrape", async (req, res) => {
+    let url = req.query.value[0];
+    let htmlString = req.query.value[1];
+    let data = await runScrape(url, htmlString);
+    res.json({ data })
+})
+
+const runScrape = async (url, htmlString) => {
+    let scraper = new Scraper(url, htmlString)
+    let browser = await scraper.startBrowser();
+    let page = await scraper.findPage(browser);
+    let data = await scraper.scrapePage(page);
+    console.log(data);
+    return data;
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
